8495d7eeebef21f784aedf00dff111608155024d
lick50001 <kazakovmisa83@yandex.ru>
2026-03-12 17:08:02 +0500

    Разработанная модель и репозиторий данных. Казаков

---
+++ app/src/main/java/com/example/notes_klimov/datas/RepoNotes.java
@@ -0,0 +1,7 @@
+package com.example.notes_klimov.datas;
+import com.example.notes_klimov.domains.models.Note;
+import java.util.ArrayList;
+
+public class RepoNotes {
+    public static ArrayList<Note> Notes = new ArrayList<>();
+}
